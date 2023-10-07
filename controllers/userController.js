5990b30abcfacd9c9d21d2050985b2805a889665
mehar sulaiman <sulaimanshabbir31@gmail.com>
2023-10-07 13:14:48 +0500

    follow/unfollow

--- controllers/userController.js
+++ controllers/userController.js
@@ -188,7 +188,7 @@ const getUserProfile = async (req, res) => {
 
     if (!user) return res.status(404).json({ error: 'User not found' });
 
-    res.status(200).json({ user });
+    res.status(200).json(user);
   } catch (error) {
     res.status(500).json({ error: error.message });
     console.log('Error in getUserProfile: ', error.message);
